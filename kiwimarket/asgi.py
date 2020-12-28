052e1dc019325a29dc0b69c607ec4403ed1c5987
Hailee <haileeyu21@gmail.com>
2020-12-28 17:55:49 +0900

    초기 세팅

--- kiwimarket/asgi.py
+++ kiwimarket/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for kiwiproject project.
+ASGI config for kiwimarket project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kiwiproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kiwimarket.settings')
 
 application = get_asgi_application()
--- kiwimarket/wsgi.py
+++ kiwimarket/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for kiwiproject project.
+WSGI config for kiwimarket project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kiwiproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kiwimarket.settings')
 
 application = get_wsgi_application()
